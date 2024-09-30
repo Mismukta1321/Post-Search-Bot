b88f97611cf1f15ee9bc7b300b5469c64af6184b
Mismukta1321 <155456446+Mismukta1321@users.noreply.github.com>
2024-09-30 23:00:59 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
 API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
+API_HASH     = "7472998b241dd149fc2b2167ce045c0e"
+BOT_TOKEN    = "7918120930:AAFcDHAfpNOKefZ8QGqAtwwMBbBFANJbgVw"
+SESSION      = "BQE60wQAnVNkfZ0mKTJnJXCvGO5p_Ih7HTtlXBlVFFKjuWCNVOVAYbTlcqbHRSLDO2WFBdJHHBkeTwyDJwWZVrcAxiFEzzWJ6E-aONstGPlbxVhg5b3yzldGBlWaQPBFHICoEqrBHPmYaGCAn4h8L5LoUqtVAGR9E2HB_gikZVmH2UylOh6mdTiWn8oQ1bWjVYTEpMHW8BLAnxcHbvvB-2PD0Ef1DwPCN1mj8oeuuHaprngsh3d3jz_CS2PWMtjHzdg2mlZ7PYRUtiEl7k5-VuG23KGWKSz3L4tyLeAabEr0Esal-F7kmMIis1O93v-V9sguwSjAYsxvx8YdWyhtm1OXzAxZ0gAAAAHiQShuAA"
+DATABASE_URI = "mongodb+srv://heraneg660:heraneg660@cluster0.whyvk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = 
 ADMIN        = 
 CHANNEL      = "RahulReviewsYT"
